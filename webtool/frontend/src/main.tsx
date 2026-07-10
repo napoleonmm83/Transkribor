dbcf82636ec34c179cd5ce47b698828dd1687998
Marcus Martini <marcusmartini83@gmail.com>
2026-07-10 10:18:41 +0200

    feat(web): react-router-Geruest, Editor in :base-Route, Stub-Seiten

--- webtool/frontend/src/main.tsx
+++ webtool/frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/ThemeProvider.tsx'
@@ -10,7 +11,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <TooltipProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
         <Toaster richColors position="bottom-right" />
       </TooltipProvider>
     </ThemeProvider>
